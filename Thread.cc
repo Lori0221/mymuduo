7b53f7d93b04e31af119df0f9f868727e84c207e
Lori0221 <1183407244@qq.com>
2025-04-05 10:28:22 +0900

    完成Thread类，主要是描述线程详细信息

---
+++ Thread.cc
@@ -0,0 +1,54 @@
+#include "Thread.h"
+#include "CurrentThread.h"
+
+#include <semaphore.h>
+
+std::atomic_int Thread::numCreated_ = 0;
+
+Thread::Thread(ThreadFunc func, const std::string &name)
+    : started_(false)
+    , joined_(false)
+    , tid_(0)
+    , func_(std::move(func))
+    , name_(name)
+{
+    setDefaultName();
+}
+
+Thread::~Thread(){
+    if(started_ && !joined_){
+        thread_->detach();  // thread类提供了设置分离线程的方法
+    }
+}
+
+// 一个Thread对象，记录的就是一个新线程的详细信息
+void Thread::start(){
+    started_ = true;
+    sem_t sem;
+    sem_init(&sem, false, 0);
+    // 开启线程
+    thread_ = std::shared_ptr<std::thread> (new std::thread([&](){
+        // 获取线程的tid
+        tid_ = CurrentThread::tid();
+        sem_post(&sem);
+        // 开启一个新线程，专门执行该线程函数
+        func_();
+    }));
+
+    // 必须等待获取上面的新线程的tid
+    sem_wait(&sem);
+}
+
+void Thread::join(){
+    joined_ = true;
+    thread_->join();
+}
+
+void Thread::setDefaultName(){
+    int num = ++numCreated_;
+    if(name_.empty()){
+        char buf[32] = {0};
+        snprintf(buf, sizeof buf, "Thread%d", name);
+        name_ = buf;
+    }
+}
